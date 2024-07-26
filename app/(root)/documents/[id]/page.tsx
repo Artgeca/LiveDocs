a482770433ebc27cbfb7a504fc76959293406092
artgeca <artgeca@gmail.com>
2024-07-26 09:44:57 +0300

    feat: edit document title feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -2,7 +2,6 @@ import CollaborativeRoom from '@/components/CollaborativeRoom';
 import { getDocument } from '@/lib/actions/room.actions';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-import { metadata } from '../../../layout';
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
   const clerkUser = await currentUser();
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,15 +4,98 @@ import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense';
 import { Editor } from '@/components/editor/Editor';
 import Header from '@/components/Header';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
+import { Input } from './ui/input';
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import { updateDocument } from '@/lib/actions/room.actions';
 
 const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
+  const currentUserType = 'editor';
+
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editting, setEditting] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  const updateTitleHandler = async (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setLoading(true);
+
+      try {
+        if (documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
+        setEditting(false);
+        updateDocument(roomId, documentTitle);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [roomId, documentTitle]);
+
+  useEffect(() => {
+    if (editting && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editting]);
+
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className="collaborative-room">
           <Header>
-            <div className="flex items-center justify-center gap-2 w-fit">
-              <p className='document-title'>Share</p>
+            <div ref={containerRef} className="flex items-center justify-center gap-2 w-fit">
+              {editting && !loading ? (
+                <Input
+                  type='text'
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder='Enter title'
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editting}
+                  className='document-title-input'
+                />
+              ) : (
+                <>
+                  <p className='document-title'>{documentTitle}</p></>
+              )}
+
+              {currentUserType === 'editor' && !editting && (
+                <Image
+                  src='/assets/icons/edit.svg'
+                  alt='edit'
+                  width={24}
+                  height={24}
+                  onClick={() => setEditting(true)}
+                  className='pointer'
+                />
+              )}
+
+              {currentUserType !== 'editor' && !editting && (
+                <p className='view-only-tag'>View only</p>
+              )}
+
+              {loading && <p className='text-sm text-gray-400'>Saving...</p>}
             </div>
             <div className="flex justify-end flex-1 w-full gap-2 sm:gap-3">
               <SignedOut>
